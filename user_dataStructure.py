728e9a898b5baa6f13c14bcb49f4558776ff4091
de_animus <de.animus12@gmail.com>
2024-06-26 20:20:36 -0500

    Prototyping user data storage

---
+++ user_dataStructure.py
@@ -0,0 +1,19 @@
+#   Where I store user/chat info
+
+
+user = { "Twitch_UserName" :
+        {
+            "voice_settings" :
+            {
+                "stability" : 123,
+                "similarity_boost" : 123,
+                "style" : 123,
+                "use_speaker_boost" : True
+            },
+            "model_id" : "<string>",
+            "twitch_follower" : False,
+            "twitch_subscriber" : False,
+            "ignored" : False
+        }
+        }
+
